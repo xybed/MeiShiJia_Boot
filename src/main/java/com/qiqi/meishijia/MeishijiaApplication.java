e619e94a575734639fce6feaa73bb6dfd7e4286c
李琪 <940608848@qq.com>
2018-06-20 18:08:41 +0800

    加入定时任务代码

--- src/main/java/com/qiqi/meishijia/MeishijiaApplication.java
+++ src/main/java/com/qiqi/meishijia/MeishijiaApplication.java
@@ -3,11 +3,13 @@ package com.qiqi.meishijia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ServletComponentScan //spring能够扫描到我们自己编写的servlet和filter
+//@EnableScheduling //开启对计划任务的支持
 public class MeishijiaApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/qiqi/meishijia/MeishijiaApplicationTests.java
+++ src/test/java/com/qiqi/meishijia/MeishijiaApplicationTests.java
@@ -1,8 +1,11 @@
 package com.qiqi.meishijia;
 
+import com.qiqi.meishijia.test.TaskExecutorConfig;
+import com.qiqi.meishijia.test.AsyncTaskService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +14,16 @@ public class MeishijiaApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    public void testAsyncTask(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+        AsyncTaskService service = context.getBean(AsyncTaskService.class);
+        for(int i=0;i<10;i++){
+            service.executeAsyncTask(i);
+            service.executeAsyncTaskPlus(i);
+        }
+        context.close();
     }
 
 }
