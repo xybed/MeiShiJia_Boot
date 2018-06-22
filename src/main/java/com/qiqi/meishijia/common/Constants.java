b9e076dce02831ea31c9548b4acf85af532f23e0
李琪 <940608848@qq.com>
2018-06-22 16:08:54 +0800

    开启https的使用，已验证抓包数据乱码

--- src/main/java/com/qiqi/meishijia/common/Constants.java
+++ src/main/java/com/qiqi/meishijia/common/Constants.java
@@ -1,7 +1,7 @@
 package com.qiqi.meishijia.common;
 
 public class Constants {
-    public static final String URL_PREFIX = "http://192.168.0.108:8080/";
+    public static final String URL_PREFIX = "https://192.168.0.108:8443/";
     public static final String FILE_BUCKET = "avatar/";
     public static final String IMG_SUFFIX = ".png";
     public static final String SLASH = "/";
