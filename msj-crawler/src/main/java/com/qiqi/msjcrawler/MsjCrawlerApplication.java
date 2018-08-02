536339c833affdf52304b6630e94ba7ae5e4fe2e
李琪 <940608848@qq.com>
2018-08-02 14:03:38 +0800

    去掉重复的DataSourceConfig,注解扫描组件

--- msj-crawler/src/main/java/com/qiqi/msjcrawler/MsjCrawlerApplication.java
+++ msj-crawler/src/main/java/com/qiqi/msjcrawler/MsjCrawlerApplication.java
@@ -2,8 +2,12 @@ package com.qiqi.msjcrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.qiqi")
+@ServletComponentScan("com.qiqi")
 public class MsjCrawlerApplication {
 
     public static void main(String[] args){
