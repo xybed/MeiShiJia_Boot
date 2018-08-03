0deb6d8094e3e07fe1bffc39bb57a1a934de826b
李琪 <940608848@qq.com>
2018-08-03 09:47:02 +0800

    后台管理模版

--- msj-back/src/main/java/com/qiqi/msjback/MsjBackApplication.java
+++ msj-back/src/main/java/com/qiqi/msjback/MsjBackApplication.java
@@ -2,8 +2,12 @@ package com.qiqi.msjback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.qiqi")
+@ServletComponentScan("com.qiqi")
 public class MsjBackApplication {
 
     public static void main(String[] args) {
