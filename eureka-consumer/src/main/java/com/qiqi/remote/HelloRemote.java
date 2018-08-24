41b1de7e71efd895e45a1c8ed58d78a8bb019611
李琪 <940608848@qq.com>
2018-08-24 14:32:30 +0800

    Eureka和Hystrix的demo

---
+++ eureka-consumer/src/main/java/com/qiqi/remote/HelloRemote.java
@@ -0,0 +1,12 @@
+package com.qiqi.remote;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "eureka-producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+}
