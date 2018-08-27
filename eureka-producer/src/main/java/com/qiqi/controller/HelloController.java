d482a13e98458886638b16fdbb0b55955acc40e8
李琪 <940608848@qq.com>
2018-08-27 17:50:44 +0800

    负载均衡

--- eureka-producer/src/main/java/com/qiqi/controller/HelloController.java
+++ eureka-producer/src/main/java/com/qiqi/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.qiqi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @RequestMapping("/hello")
     public String hello(@RequestParam String name){
-        return "hello "+name+"，this is first message";
+        return "hello "+name+"，this is first message，serverPort:"+serverPort;
     }
 }
