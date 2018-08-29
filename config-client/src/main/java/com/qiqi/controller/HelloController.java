86613115033a6e761a88dae6eb618a0f504f5f4b
李琪 <940608848@qq.com>
2018-08-29 13:52:55 +0800

    配置中心client端

---
+++ config-client/src/main/java/com/qiqi/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.qiqi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${77.name}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
