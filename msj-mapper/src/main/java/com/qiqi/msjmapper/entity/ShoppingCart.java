40fe61e496779857f8681b27e45868cc08b2740a
李琪 <940608848@qq.com>
2018-11-06 14:07:46 +0800

    添加shopping_cart的映射

---
+++ msj-mapper/src/main/java/com/qiqi/msjmapper/entity/ShoppingCart.java
@@ -0,0 +1,116 @@
+package com.qiqi.msjmapper.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ShoppingCart {
+    private Integer id;
+
+    private Integer productId;
+
+    private String name;
+
+    private String image;
+
+    private BigDecimal price;
+
+    private BigDecimal originalPrice;
+
+    private BigDecimal discountPrice;
+
+    private Integer num;
+
+    private Integer status;
+
+    private Date gmtCreate;
+
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(BigDecimal originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public BigDecimal getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(BigDecimal discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
