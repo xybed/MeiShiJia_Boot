284c143de54ebe6254fc98d41982cc9f724e13c8
7mu <940608848@qq.com>
2018-11-06 19:40:23 +0800

    先删除mapper的配置yml

---
+++ msj-mapper/src/main/java/com/qiqi/msjmapper/dto/ShoppingCartDto.java
@@ -0,0 +1,85 @@
+package com.qiqi.msjmapper.dto;
+
+import java.math.BigDecimal;
+
+public class ShoppingCartDto {
+    private Integer id;
+
+    private Integer productId;
+
+    private String name;
+
+    private String image;
+
+    private BigDecimal price;
+
+    private BigDecimal originalPrice;
+
+    private BigDecimal discountPrice;
+
+    private Integer num;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(BigDecimal originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public BigDecimal getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(BigDecimal discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+}
---
+++ msj-mapper/src/main/java/com/qiqi/msjmapper/mapper/ShoppingCartCustomMapper.java
@@ -0,0 +1,10 @@
+package com.qiqi.msjmapper.mapper;
+
+import com.qiqi.msjmapper.dto.ShoppingCartDto;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ShoppingCartCustomMapper {
+    List<ShoppingCartDto> queryShoppingCart(@Param("user_id") Integer userId, @Param("status") Integer status);
+}
