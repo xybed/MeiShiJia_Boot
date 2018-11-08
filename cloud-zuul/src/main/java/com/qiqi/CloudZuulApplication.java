72dc011e3d009c590a4da88c6324600898f48108
李琪 <940608848@qq.com>
2018-11-08 10:24:01 +0800

    网关配置

--- cloud-zuul/src/main/java/com/qiqi/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/com/qiqi/CloudZuulApplication.java
@@ -3,10 +3,12 @@ package com.qiqi;
 import com.qiqi.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class CloudZuulApplication {
 
