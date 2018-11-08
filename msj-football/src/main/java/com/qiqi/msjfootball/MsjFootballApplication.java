96cdfcb80bad722dc79be9a94f437f06c47b1b5d
李琪 <940608848@qq.com>
2018-11-08 10:03:45 +0800

    足球服务加入consul注册中心

--- msj-football/src/main/java/com/qiqi/msjfootball/MsjFootballApplication.java
+++ msj-football/src/main/java/com/qiqi/msjfootball/MsjFootballApplication.java
@@ -3,11 +3,13 @@ package com.qiqi.msjfootball;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.qiqi")
 @ServletComponentScan("com.qiqi")
+@EnableDiscoveryClient
 public class MsjFootballApplication {
 
     public static void main(String[] args) {
